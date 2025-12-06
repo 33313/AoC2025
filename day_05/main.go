a7633c4c4d0f0ab2f4370874aa7b948cf7d3ead0
33313 <84045642+33313@users.noreply.github.com>
2025-12-06 18:56:51 +0100

    day 05 (2/2)

--- day_05/main.go
+++ day_05/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -11,7 +12,7 @@ import (
 func main() {
 	f, _ := os.Open("input.txt")
 	defer f.Close()
-	var ranges []string
+	var ranges [][]int64
 	var products []string
 	scanner := bufio.NewScanner(f)
 	is_listing_products := false
@@ -24,26 +25,22 @@ func main() {
 		if is_listing_products {
 			products = append(products, text)
 		} else {
-			ranges = append(ranges, text)
+			r := strings.Split(text, "-")
+			start, _ := strconv.Atoi(r[0])
+			stop, _ := strconv.Atoi(r[1])
+			ranges = append(ranges, []int64{int64(start), int64(stop)})
 		}
 	}
 	part_01(ranges, products)
-	part_02(ranges, products)
+	part_02(ranges)
 }
 
-func part_01(ranges []string, products []string) {
+func part_01(ranges [][]int64, products []string) {
 	fresh := 0
-	var rngs [][]int
-	for _, v := range ranges {
-		r := strings.Split(v, "-")
-		start, _ := strconv.Atoi(r[0])
-		stop, _ := strconv.Atoi(r[1])
-		rngs = append(rngs, []int{start, stop})
-	}
 	for _, p := range products {
 		product, _ := strconv.Atoi(p)
-		for _, v := range rngs {
-			if v[0] <= product && product <= v[1] {
+		for _, v := range ranges {
+			if v[0] <= int64(product) && int64(product) <= v[1] {
 				fresh++
 				break
 			}
@@ -52,7 +49,26 @@ func part_01(ranges []string, products []string) {
 	fmt.Printf("Part 1: %v\n", fresh)
 }
 
-func part_02(ranges []string, products []string) {
-	fresh := 0
+func part_02(ranges [][]int64) {
+	var fresh int64
+	sort.Slice(ranges, func(i, j int) bool {
+		return ranges[i][0] < ranges[j][0]
+	})
+	start := ranges[0][0]
+	end := ranges[0][1]
+	for i := 1; i < len(ranges); i++ {
+		next_start := ranges[i][0]
+		next_end := ranges[i][1]
+		if next_start <= end {
+			if next_end > end {
+				end = next_end
+			}
+		} else {
+			fresh += end - start + 1
+			start = next_start
+			end = next_end
+		}
+	}
+	fresh += end - start + 1
 	fmt.Printf("Part 2: %v\n", fresh)
 }
