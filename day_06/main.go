d0bc811b8033f1fb5730153ee7df82f2b41e22a3
33313 <84045642+33313@users.noreply.github.com>
2025-12-07 20:00:43 +0100

    day 06 (2/2)

--- day_06/main.go
+++ day_06/main.go
@@ -4,19 +4,28 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"slices"
 	"strconv"
 	"strings"
 )
+
+func reverse(s string) string {
+	runes := []rune(s)
+	slices.Reverse(runes)
+	return string(runes)
+}
 
 func main() {
 	f, _ := os.Open("input.txt")
 	defer f.Close()
 
+	var lines []string
 	var signs []string
 	var numbers [][]int
 
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
+		lines = append(lines, reverse(scanner.Text()))
 		text := strings.Fields(scanner.Text())
 		if _, err := strconv.Atoi(text[0]); err != nil {
 			signs = text
@@ -32,7 +41,7 @@ func main() {
 	}
 
 	part_01(numbers, signs)
-	part_02(numbers, signs)
+	part_02(lines)
 }
 
 func part_01(numbers [][]int, signs []string) {
@@ -54,7 +63,56 @@ func part_01(numbers [][]int, signs []string) {
 	fmt.Printf("Part 1: %v\n", sum)
 }
 
-func part_02(numbers [][]int, signs []string) {
+func part_02(lines []string) {
 	var sum int
+	nums := lines[:len(lines)-1]
+	signs := lines[len(lines)-1]
+
+	gaps := make(map[int]bool)
+	var last_rune rune = ' '
+	for i, v := range signs {
+		if last_rune == '*' || last_rune == '+' {
+			gaps[i] = true
+		}
+		last_rune = v
+	}
+
+	parsed := make([]string, len(signs))
+	for _, ln := range nums {
+		for pos, char := range ln {
+			if gaps[pos] {
+				continue
+			}
+			parsed[pos] += string(char)
+		}
+	}
+
+	temp := []string{}
+	for i := range parsed {
+		if gaps[i] {
+			temp = []string{}
+			continue
+		}
+		temp = append(temp, parsed[i])
+		if signs[i] == '*' {
+			temp_sum := 1
+			for _, n_str := range temp {
+				n_str = strings.TrimSpace(n_str)
+				n, _ := strconv.Atoi(n_str)
+				temp_sum *= n
+			}
+			sum += temp_sum
+			continue
+		} else if signs[i] == '+' {
+			temp_sum := 0
+			for _, n_str := range temp {
+				n_str = strings.TrimSpace(n_str)
+				n, _ := strconv.Atoi(n_str)
+				temp_sum += n
+			}
+			sum += temp_sum
+			continue
+		}
+	}
 	fmt.Printf("Part 2: %v\n", sum)
 }
